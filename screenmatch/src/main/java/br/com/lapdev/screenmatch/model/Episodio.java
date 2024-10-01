7395f34144d23d8628779d5d29458dc78ce0d909
Lerry Augusto <lerry.pirani@gmail.com>
2024-10-01 12:31:08 -0300

    mapeando relacionamentos

--- screenmatch/src/main/java/br/com/lapdev/screenmatch/model/Episodio.java
+++ screenmatch/src/main/java/br/com/lapdev/screenmatch/model/Episodio.java
@@ -1,21 +1,34 @@
 package br.com.lapdev.screenmatch.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+@Entity
+@Table(name = "episodios")
 public class Episodio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Integer temporada;
     private String titulo;
     private Integer numeroEpisodio;
     private Double avaliacao;
     private LocalDate dataLancamento;
 
-    public  Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio){
+    @ManyToOne
+    private Serie serie;
+
+    public Episodio() {
+    }
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
-        this.titulo  = dadosEpisodio.titulo();
+        this.titulo = dadosEpisodio.titulo();
         this.numeroEpisodio = dadosEpisodio.numero();
         try {
-            this.avaliacao = Double.valueOf( dadosEpisodio.avaliacao());
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
         } catch (NumberFormatException e) {
             this.avaliacao = 0.0;
         }
@@ -24,6 +37,14 @@ public class Episodio {
         } catch (DateTimeParseException e) {
             this.dataLancamento = null;
         }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Integer getTemporada() {
@@ -64,6 +85,14 @@ public class Episodio {
 
     public void setDataLancamento(LocalDate dataLancamento) {
         this.dataLancamento = dataLancamento;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
     }
 
     @Override
