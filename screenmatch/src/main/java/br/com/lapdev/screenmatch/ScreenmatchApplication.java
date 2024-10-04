14fbb5c4fb5bd4e3f740ebb7b71c99232f121e38
Lerry Augusto <lerry.pirani@gmail.com>
2024-10-03 22:50:24 -0300

    fornecendo dados para o front

--- screenmatch/src/main/java/br/com/lapdev/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/lapdev/screenmatch/ScreenmatchApplication.java
@@ -1,25 +1,13 @@
 package br.com.lapdev.screenmatch;
 
-import br.com.lapdev.screenmatch.principal.Principal;
-import br.com.lapdev.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication {
 
-	@Autowired
-	private SerieRepository repositorio;
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
-	}
 }
---
+++ screenmatch/src/main/java/br/com/lapdev/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,25 @@
+//package br.com.lapdev.screenmatch;
+//
+//import br.com.lapdev.screenmatch.principal.Principal;
+//import br.com.lapdev.screenmatch.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
+//
+//	@Autowired
+//	private SerieRepository repositorio;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Principal principal = new Principal(repositorio);
+//		principal.exibeMenu();
+//	}
+//}
