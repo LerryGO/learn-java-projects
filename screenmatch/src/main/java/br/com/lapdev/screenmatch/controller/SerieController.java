09054d00a21757ed8756e0346e152e70901b05c5
Lerry Augusto <lerry.pirani@gmail.com>
2024-10-09 16:24:07 -0300

    Detalhando o screenmatch

--- screenmatch/src/main/java/br/com/lapdev/screenmatch/controller/SerieController.java
+++ screenmatch/src/main/java/br/com/lapdev/screenmatch/controller/SerieController.java
@@ -1,6 +1,7 @@
 package br.com.lapdev.screenmatch.controller;
 
 import br.com.lapdev.screenmatch.dto.SerieDTO;
+import br.com.lapdev.screenmatch.dto.EpisodioDTO;
 import br.com.lapdev.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,6 +39,21 @@ public class SerieController {
     public SerieDTO obterPorId(@PathVariable Long id){
         return servico.obterPorId(id);
     };
+
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id){
+        return servico.obterTodasTemporadas(id);
+    }
+
+    @GetMapping("/{id}/temporadas/{numero}")
+    public List<EpisodioDTO> obterTemporadasPorNumero(@PathVariable Long id,@PathVariable Long numero){
+        return servico.obterTemporadasPorNumero(id, numero );
+    }
+
+    @GetMapping("/categoria/{nomeGenero}")
+    public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero){
+        return  servico.obterSeriesPorCategoria(nomeGenero);
+    }
 
 
 }
--- screenmatch/src/main/java/br/com/lapdev/screenmatch/service/SerieService.java
+++ screenmatch/src/main/java/br/com/lapdev/screenmatch/service/SerieService.java
@@ -1,6 +1,8 @@
 package br.com.lapdev.screenmatch.service;
 
+import br.com.lapdev.screenmatch.dto.EpisodioDTO;
 import br.com.lapdev.screenmatch.dto.SerieDTO;
+import br.com.lapdev.screenmatch.model.Categoria;
 import br.com.lapdev.screenmatch.model.Serie;
 import br.com.lapdev.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +44,28 @@ public class SerieService {
             return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
         }
         return null;
+    }
+
+    public List<EpisodioDTO> obterTodasTemporadas(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return s.getEpisodios().stream()
+                    .map(e -> new EpisodioDTO(e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()))
+                    .collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    public List<EpisodioDTO> obterTemporadasPorNumero(Long id, Long numero) {
+        return repositorio.obterEpisodiosPorTemporada(id, numero).stream()
+                .map(e -> new EpisodioDTO(e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterSeriesPorCategoria(String nomeGenero) {
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        return converteDados(repositorio.findByGenero(categoria));
     }
 }
