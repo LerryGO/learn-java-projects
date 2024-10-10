a522aa9fa4bd55c9e72655125512d269a4b54979
Lerry Augusto <lerry.pirani@gmail.com>
2024-10-10 17:48:30 -0300

    Criando projeto Med Voll- CRUD Spring web

---
+++ med-voll-api/src/test/java/br/com/lapdev/med/voll/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lapdev.med.voll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
