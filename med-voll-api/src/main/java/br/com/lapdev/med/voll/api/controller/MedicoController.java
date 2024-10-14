cfb357e5054bdf4d8efed32215a6fbab6d8c2dad
Lerry Augusto <lerry.pirani@gmail.com>
2024-10-14 15:15:06 -0300

    Spring Data JPA - Migrations

--- med-voll-api/src/main/java/br/com/lapdev/med/voll/api/controller/MedicoController.java
+++ med-voll-api/src/main/java/br/com/lapdev/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package br.com.lapdev.med.voll.api.controller;
 
 import br.com.lapdev.med.voll.api.medico.DadosCadastroMedico;
+import br.com.lapdev.med.voll.api.medico.Medico;
+import br.com.lapdev.med.voll.api.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
     }
 }
