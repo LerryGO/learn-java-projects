63af394bf474ac0bb93eb65d3455aa904ffb4519
Lerry Augusto <lerry.pirani@gmail.com>
2024-10-14 16:11:27 -0300

    Requisições GET

--- med-voll-api/src/main/java/br/com/lapdev/med/voll/api/controller/MedicoController.java
+++ med-voll-api/src/main/java/br/com/lapdev/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,19 @@
 package br.com.lapdev.med.voll.api.controller;
 
 import br.com.lapdev.med.voll.api.medico.DadosCadastroMedico;
+import br.com.lapdev.med.voll.api.medico.DadosListagemMedico;
 import br.com.lapdev.med.voll.api.medico.Medico;
 import br.com.lapdev.med.voll.api.medico.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +26,13 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    // EX URL: .../medicos?size=10&page=1
+    // ORDENACAO por URL: .../medicos?sort=crm,desc
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        var medicos = repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(medicos);
     }
 }
