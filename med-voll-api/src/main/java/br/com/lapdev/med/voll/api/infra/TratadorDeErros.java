1bc9a127a905918bdba5836c9b29ab65bf39b80f
Lerry Augusto <lerry.pirani@gmail.com>
2024-10-16 13:45:32 -0300

    tratando erro 400

--- med-voll-api/src/main/java/br/com/lapdev/med/voll/api/infra/TratadorDeErros.java
+++ med-voll-api/src/main/java/br/com/lapdev/med/voll/api/infra/TratadorDeErros.java
@@ -2,14 +2,30 @@ package br.com.lapdev.med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TratadorDeErros {
 
+    // Método de erro 404 - ID ou caminho não encontrado
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratarErro404(){
+    public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    // Método de erro 400 - Erro de validação
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValicacao::new).toList());
+    }
+
+    private record DadosErroValicacao(String campo, String mensagem) {
+        public DadosErroValicacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
